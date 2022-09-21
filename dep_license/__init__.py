48b2dbdc65e7af2f469ab378c1208d309a7adf0d
Martin Jespersen <martincjespersen@gmail.com>
2022-09-21 19:46:53 +0200

    fixed small bug in classifier with multiple licenses

--- dep_license/__init__.py
+++ dep_license/__init__.py
@@ -131,7 +131,7 @@ def worker(d):
             license_class.add("::".join([x.strip() for x in c.split("::")[1:]]))
 
     license_class_str = (
-        license_class.pop() if len(license_class) == 1 else str(license_class)
+        license_class.pop() if len(license_class) == 1 else ', '.join(license_class)
     )
     record.append(license_class_str)
 
